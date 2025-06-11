64e66af9e9676f528066ce0d80441e49b753f452
m3GA328P <sebasfares12@gmail.com>
2025-06-10 23:57:37 -0500

    Update Dueno.java

--- src/main/java/com/utp/integradorspringboot/models/Dueno.java
+++ src/main/java/com/utp/integradorspringboot/models/Dueno.java
@@ -15,5 +15,59 @@ public class Dueno {
     private String direccion;
     private String relacionConMascota;
 
-    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDniRuc() {
+        return dniRuc;
+    }
+
+    public void setDniRuc(String dniRuc) {
+        this.dniRuc = dniRuc;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRelacionConMascota() {
+        return relacionConMascota;
+    }
+
+    public void setRelacionConMascota(String relacionConMascota) {
+        this.relacionConMascota = relacionConMascota;
+    }
 }
