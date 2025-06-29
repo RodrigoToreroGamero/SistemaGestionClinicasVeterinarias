98efb3fbf6ef34e0363f3bea55f010b22a4adfac
Rodrigo Tejada <rodrigotejada728@gmail.com>
2025-06-28 22:09:52 -0500

    Creacion de clases
    Cree algunas de las clases

---
+++ src/main/java/com/utp/integradorspringboot/models/Clinica.java
@@ -0,0 +1,151 @@
+package com.utp.integradorspringboot.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "`clinica_veterinaria`") // usar singular y proteger con backticks si usas MySQL 
+public class Clinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nombre_clinica")
+    private String nombre_clinica;
+
+    @Column(name = "ruc")
+    private String ruc;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "link_web")
+    private String link_web;
+
+    @ManyToOne
+    @JoinColumn(name = "id_veterinario", nullable = false)
+    private Veterinario veterinario;
+
+    @Column(name = "plan_suscripcion")
+    private String plan_suscripcion;
+
+    @Column(name = "pasarela_pago")
+    private String pasarela_pago;
+
+    // Constructors
+    public Clinica() {
+    }
+
+    public Clinica(Long id, String nombre_clinica, String ruc, String direccion, String telefono, String link_web, Veterinario veterinario, String plan_suscripcion, String pasarela_pago) {
+        this.id = id;
+        this.nombre_clinica = nombre_clinica;
+        this.ruc = ruc;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.link_web = link_web;
+        this.veterinario = veterinario;
+        this.plan_suscripcion = plan_suscripcion;
+        this.pasarela_pago = pasarela_pago;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre_clinica() {
+        return nombre_clinica;
+    }
+
+    public void setNombre_clinica(String nombre_clinica) {
+        this.nombre_clinica = nombre_clinica;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getLink_web() {
+        return link_web;
+    }
+
+    public void setLink_web(String link_web) {
+        this.link_web = link_web;
+    }
+
+    public Veterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(Veterinario veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public String getPlan_suscripcion() {
+        return plan_suscripcion;
+    }
+
+    public void setPlan_suscripcion(String plan_suscripcion) {
+        this.plan_suscripcion = plan_suscripcion;
+    }
+
+    public String getPasarela_pago() {
+        return pasarela_pago;
+    }
+
+    public void setPasarela_pago(String pasarela_pago) {
+        this.pasarela_pago = pasarela_pago;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Clinica)) {
+            return false;
+        }
+        Clinica other = (Clinica) object;
+        if((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Clinica{" + "id=" + id + ", nombre_clinica=" + nombre_clinica + ", ruc=" + ruc + ", direccion=" + direccion + ", telefono=" + telefono + ", link_web=" + link_web + ", veterinario=" + veterinario + ", plan_suscripcion=" + plan_suscripcion + ", pasarela_pago=" + pasarela_pago +'}';
+    }
+}
---
+++ src/main/java/com/utp/integradorspringboot/models/Veterinario.java
@@ -0,0 +1,115 @@
+package com.utp.integradorspringboot.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "veterinario")
+public class Veterinario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @Column(name = "nombre_colegio_medico")
+    private String nombre_colegio_medico;
+
+    @Column(name = "especialidad")
+    private String especialidad;
+
+    @Column(name = "horario_laboral")
+    private String horario_laboral;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_clinica", nullable = false)
+    private Clinica clinica;
+
+    // Constructors
+    public Veterinario() {
+    }
+
+    public Veterinario(String nombre_colegio_medico, String especialidad, String horario_laboral, Clinica clinica, Usuario usuario) {
+        this.nombre_colegio_medico = nombre_colegio_medico;
+        this.especialidad = especialidad;
+        this.horario_laboral = horario_laboral;
+        this.clinica = clinica;
+        this.usuario = usuario;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre_colegio_medico() {
+        return nombre_colegio_medico;
+    }
+
+    public void setNombre_colegio_medico(String nombre_colegio_medico) {
+        this.nombre_colegio_medico = nombre_colegio_medico;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getHorario_laboral() {
+        return horario_laboral;
+    }
+
+    public void setHorario_laboral(String horario_laboral) {
+        this.horario_laboral = horario_laboral;
+    }
+
+    public Clinica getClinica() {
+        return clinica;
+    }
+
+    public void setClinica(Clinica clinica) {
+        this.clinica = clinica;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Veterinario)) {
+            return false;
+        }
+        Veterinario other = (Veterinario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinario{" + "id=" + id + ", nombre_colegio_medico=" + nombre_colegio_medico + ", especialidad=" + especialidad + ", horario_laboral=" + horario_laboral + ", clinica=" + clinica + ", usuario=" + usuario + '}';
+    }
+}
