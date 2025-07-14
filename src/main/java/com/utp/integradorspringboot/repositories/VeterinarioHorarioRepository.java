373df0ec74f30de6571d2f49bc99a7adb7dccd72
Rodrigo Tejada <rodrigotejada728@gmail.com>
2025-07-14 18:00:20 -0500

    Vista de Veterinarios

--- src/main/java/com/utp/integradorspringboot/repositories/VeterinarioHorarioRepository.java
+++ src/main/java/com/utp/integradorspringboot/repositories/VeterinarioHorarioRepository.java
@@ -1,6 +1,10 @@
 package com.utp.integradorspringboot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.utp.integradorspringboot.models.Veterinario_horario;
@@ -8,4 +12,6 @@ import com.utp.integradorspringboot.models.Veterinario_horario;
 @Repository
 public interface VeterinarioHorarioRepository extends JpaRepository<Veterinario_horario, Long> {
     // Las operaciones CRUD básicas son proporcionadas automáticamente por JpaRepository
+    @Query("SELECT vh FROM Veterinario_horario vh WHERE vh.veterinario.id = :veterinarioId")
+    List<Veterinario_horario> findByVeterinarioId(@Param("veterinarioId") Long veterinarioId);
 } 
